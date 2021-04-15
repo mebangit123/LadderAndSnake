2ccfcb9557acda1cde4f29e603f4169731507337
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 08:46:37 +0530

    Added second player to play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,43 +1,84 @@
 
 public class SnakeAndLadder {
 	public static void main(String[] args) {
-		System.out.println("Snake And Ladder Simulator Problem");
+		System.out.println("Snake & Ladder Simulator");
 
-		int win = 100;
-		int player= 0 ;
-		int start = 0;
-		int ladder = 1;
-		int snake = 2;
-		int no_times_roll = 0;
+		int win=100;
+		int score=0;
+		int player1=0;
+		int player2=0;
+		int ladder=1;
+		int snake=2;
+		int start=0;
+		int die_position=0;
+		int no_of_times_roll=0;
 
-
-		while(player < win)
+		while(player1 < win & player2 < win)
 		{
-			int options = (int)Math.floor(Math.random() * 10) % 3;
-			int roll_die = (int)Math.floor((Math.random() * 10) % 6) + 1;
+			int p1=1;
+			int p2=2;
+			int player=(int)Math.floor((Math.random() * 10) % 3)+1;
+			int options=(int)Math.floor(Math.random() * 10) % 3;
+			int roll_dice=(int)Math.floor((Math.random() * 10) % 6)+1;
+
+			if(player == p1)
+			{
 				if(options == ladder)
 				{
-					int check = player + roll_die;
-					if(check > win)
-						player +=0;
+					int check = player1 + roll_dice;
+					if(check >  win)
+					{
+						player1+=0;
+					}
 					else
-						player += roll_die;
+					{
+						player1+=roll_dice;
+					}
 				}
-
 				else if(options == snake)
 				{
-					player = player - roll_die;
+					player1 = player1 - roll_dice;
 				}
-
 				else
 				{
-					player += 0;
+					player1+=0;
 				}
-			System.out.println("position =" +player);
-			no_times_roll++;
-		}
-		System.out.println("You have the game");
-		System.out.println("Number of Times Die is roll :"+no_times_roll);
+			}
+			else if(player == p2)
+			{
+				if(options == ladder)
+				{
+					int check = player2 + roll_dice;
+					if(check >  win)
+					{
+						player2+=0;
+					}
+					else
+					{
+						player2+=roll_dice;
+					}
+				}
+				else if(options == snake)
+				{
+					player2 = player2 - roll_dice;
+				}
+				else
+				{
+					player2+=0;
+				}
+			}
+				no_of_times_roll++;
+			}
 
+		System.out.println("No of times dice was played: "+ no_of_times_roll);
+
+		if(player1 == win)
+		{
+			System.out.println("Player1 win");
+		}
+		else if(player2 == win)
+		{
+			System.out.println("Player2 win");
+		}
 	}
 }
